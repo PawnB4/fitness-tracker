e852f3ca8651e21f6e21c801d9ed8741aa25cca4
PawnB4 <francomartinpascual@gmail.com>
2025-03-08 01:02:05 -0300

    Enable Write-Ahead Logging (WAL) for improved database performance

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,9 +1,11 @@
 import { drizzle } from "drizzle-orm/expo-sqlite";
-import { openDatabaseSync} from "expo-sqlite";
+import { openDatabaseSync } from "expo-sqlite";
 
 export const fitnessTrackerDb = openDatabaseSync("fitness-tracker.db", { enableChangeListener: true });
 
-// Enable foreign key constraints
 fitnessTrackerDb.execAsync("PRAGMA foreign_keys = ON;");
+
+fitnessTrackerDb.execAsync('PRAGMA journal_mode = WAL;');
+
 
 export const db = drizzle(fitnessTrackerDb, { casing: "snake_case" });
