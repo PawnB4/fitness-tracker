05e4183c6145af18d67070023db3212c9cb56c96
PawnB4 <francomartinpascual@gmail.com>
2025-06-27 21:33:25 -0300

    implemented bar chart

--- app/(tabs)/progress.tsx
+++ app/(tabs)/progress.tsx
@@ -3,7 +3,7 @@ import { useFont } from "@shopify/react-native-skia";
 import { asc } from "drizzle-orm";
 import { useLiveQuery } from "drizzle-orm/expo-sqlite";
 import { View } from "react-native";
-import { CartesianChart, Line } from "victory-native";
+import { Bar, CartesianChart } from "victory-native";
 import { Text } from "~/components/ui/text";
 import { db } from "~/db/drizzle";
 import * as schema from "~/db/schema";
@@ -21,57 +21,68 @@ export default function Page() {
 			.orderBy(asc(schema.workouts.createdAt)),
 	);
 
-	const MONTHLY_WORKOUTS = (() => {
-		const months = [
-			"Jan",
-			"Feb",
-			"Mar",
-			"Apr",
-			"May",
-			"Jun",
-			"Jul",
-			"Aug",
-			"Sep",
-			"Oct",
-			"Nov",
-			"Dec",
-		];
+	// const MONTHLY_WORKOUTS = (() => {
+	// 	const months = [
+	// 		"Jan",
+	// 		"Feb",
+	// 		"Mar",
+	// 		"Apr",
+	// 		"May",
+	// 		"Jun",
+	// 		"Jul",
+	// 		"Aug",
+	// 		"Sep",
+	// 		"Oct",
+	// 		"Nov",
+	// 		"Dec",
+	// 	];
 
-		const monthCounts = months.reduce(
-			(acc, month) => {
-				acc[month] = 0;
-				return acc;
-			},
-			{} as Record<string, number>,
-		);
+	// 	const monthCounts = months.reduce(
+	// 		(acc, month) => {
+	// 			acc[month] = 0;
+	// 			return acc;
+	// 		},
+	// 		{} as Record<string, number>,
+	// 	);
 
-		const currentYear = new Date().getFullYear();
+	// 	const currentYear = new Date().getFullYear();
 
-		workouts?.forEach((workout) => {
-			const workoutDate = new Date(workout.createdAt ?? "");
-			const workoutYear = workoutDate.getFullYear();
-			
-			// Only count workouts from the current year
-			if (workoutYear === currentYear) {
-				const month = workoutDate.toLocaleDateString("en-US", { month: "short" });
-				if (Object.prototype.hasOwnProperty.call(monthCounts, month)) {
-					monthCounts[month]++;
-				}
-			}
-		});
+	// 	workouts?.forEach((workout) => {
+	// 		const workoutDate = new Date(workout.createdAt ?? "");
+	// 		const workoutYear = workoutDate.getFullYear();
 
-		return months.map((month) => ({
-			month,
-			count: monthCounts[month],
-		}));
-	})();
+	// 		// Only count workouts from the current year
+	// 		if (workoutYear === currentYear) {
+	// 			const month = workoutDate.toLocaleDateString("en-US", { month: "short" });
+	// 			if (Object.prototype.hasOwnProperty.call(monthCounts, month)) {
+	// 				monthCounts[month]++;
+	// 			}
+	// 		}
+	// 	});
+
+	// 	return months.map((month) => ({
+	// 		month,
+	// 		count: monthCounts[month],
+	// 	}));
+
+	// })();
+
+	const MONTHLY_WORKOUTS = [
+		{ month: "Jan", count: 8 },
+		{ month: "Feb", count: 12 },
+		{ month: "Mar", count: 15 },
+		{ month: "Apr", count: 11 },
+		{ month: "May", count: 16 },
+		{ month: "Jun", count: 14 },
+		{ month: "Jul", count: 9 },
+		{ month: "Aug", count: 13 },
+		{ month: "Sep", count: 7 },
+		{ month: "Oct", count: 10 },
+		{ month: "Nov", count: 6 },
+		{ month: "Dec", count: 4 },
+	];
 
 	console.log(MONTHLY_WORKOUTS);
-
-	const DATA = Array.from({ length: 31 }, (_, i) => ({
-		day: i,
-		highTmp: 40 + 30 * Math.random(),
-	}));
 
 	return (
 		<View className="mx-auto h-[300px] w-4/5">
@@ -82,13 +93,25 @@ export default function Page() {
 				data={MONTHLY_WORKOUTS}
 				xKey="month"
 				yKeys={["count"]}
-				axisOptions={{
+				viewport={{ x: [0, 11], y: [0, 16] }}
+				padding={10}
+				xAxis={{
 					font: font,
 					tickCount: 12,
 				}}
+				// yAxis={{
+				// 	font: font,
+				// 	tickCount: 12,
+				// }}
 			>
-				{({ points }) => (
-					<Line points={points.count} color="red" strokeWidth={3} />
+				{({ points, chartBounds }) => (
+					// <Line points={points.count} color="red" strokeWidth={3} />
+					<Bar
+						points={points.count}
+						chartBounds={chartBounds}
+						color="red"
+						roundedCorners={{ topLeft: 10, topRight: 10 }}
+					/>
 				)}
 			</CartesianChart>
 		</View>
