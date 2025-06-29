7104698dd35e2f23cf04ae98376ef761d035798d
PawnB4 <francomartinpascual@gmail.com>
2025-06-29 19:49:20 -0300

    Installed necessary packages

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "drizzle-kit";
+
+export default {
+	dialect: "sqlite",
+	schema: "./db/schema.ts",
+	casing: "snake_case",
+	out: "./drizzle",
+	driver: "expo",
+} satisfies Config;
